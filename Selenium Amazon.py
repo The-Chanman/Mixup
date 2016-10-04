21754775f3661057cd2bfe4edd158ea919d53491
The-Chanman <ecphylo@gmail.com>
2016-10-04 16:08:29 -0700

    writing to result file

--- Selenium Amazon.py
+++ Selenium Amazon.py
@@ -1,4 +1,5 @@
 import sys
+import Config
 import requests
 import time
 import signal
@@ -29,5 +30,17 @@ time.sleep(3)
 #assert "Python" in driver.title
 resultList = driver.find_elements_by_class_name("s-result-item")
 
-firstItem = resultList[randint(0,9)].find_elements_by_class_name("a-link-normal")
-print firstItem[0].get_attribute('href')
+# URLlist = []
+f = open("result.txt","w") #opens file with name of "test.txt"
+f.close()
+f = open("result.txt","a")
+
+for result in resultList:
+	item = result.find_elements_by_class_name("a-link-normal")
+	f.write(item[0].get_attribute('href')+"\n")
+	# URLlist.append(item[0].get_attribute('href'))
+
+f.close()
+driver.close()
+# firstItem = resultList[randint(0,9)].find_elements_by_class_name("a-link-normal")
+# print firstItem[0].get_attribute('href')
