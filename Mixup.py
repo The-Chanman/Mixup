c03d73675d1f6293060c3e69aaed2e4e483eb5d6
The-Chanman <ecphylo@gmail.com>
2016-10-04 12:46:19 -0700

    Looks like Amazon refuses services

--- Mixup.py
+++ Mixup.py
@@ -5,9 +5,21 @@ import requests
 print 'A lookup of:', len(sys.argv), 'products.'
 print 'Product List:', str(sys.argv)
 
-productList = str(sys.argv)
+productList = (sys.argv)
+del productList[0]
+print 'Product List:', productList
+
+htmlProductFormat = []
 
 for product in productList:
-	product.replace(" ", "-")
+	htmlProductFormat.append(product.replace(" ", "-"))
 
-print productList
+print htmlProductFormat
+
+page = requests.get('https://www.amazon.com/s/&field-keywords=', htmlProductFormat[0])
+print page
+
+tree = html.fromstring(page.content)
+
+productOptions = tree.xpath('//div[@id="atfResults"]')
+print productOptions
